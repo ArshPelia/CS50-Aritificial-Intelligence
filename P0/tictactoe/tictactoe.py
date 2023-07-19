0c0e25a817f7058bcf16f1ba06a2dceaf4d25cf8
arshjit pelia <arsh.pelia1@gmail.com>
2023-07-19 15:36:39 -0400

    added player function

--- P0/tictactoe/tictactoe.py
+++ P0/tictactoe/tictactoe.py
@@ -17,13 +17,28 @@ def initial_state():
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 
-
 def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    # raise NotImplementedError
+    count = 0
+    for row in board:
+        for element in row:
+            if element is EMPTY:
+                count += 1
 
+    print(count)
+
+    if count == 9:
+        return X 
+    
+    if count % 2 == 0: 
+        return O
+    else:
+        return X 
+    
+    
 
 def actions(board):
     """
